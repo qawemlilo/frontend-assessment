ec19f087944d3e1dc4d9d7b33cfdc1ae18aeec48
ikhlaas21 <ikhlaas21.rawoot@gmail.com>
2023-10-08 23:13:58 +0200

    1st

--- src/App.vue
+++ src/App.vue
@@ -3,14 +3,21 @@ import Search from './components/Search.vue'
 </script>
 
 <template>
-  <div>
-    <h1>
-      MovieDB Search Engine
-    </h1>
+  <div class="landing">
+   
     <Search />
   </div>
 </template>
 
 <style scoped>
+.landing{
+    background-image: url('https://i.postimg.cc/VLG9zSLH/148561614.jpg');
+   
+    width: auto;
+    min-height: 100vh;
+    padding-bottom: 20vh;
+   
 
+    background-position: bottom;
+}
 </style>
--- src/components/ShowMovie.vue
+++ src/components/ShowMovie.vue
@@ -1,3 +1,56 @@
 <template>
-  
+  <div>
+    <!-- Bootstrap Modal -->
+    <div
+      class="modal fade"
+      :id="'movieModal' + movie.id"
+      tabindex="-1"
+      role="dialog"
+      aria-labelledby="movieModalLabel"
+      aria-hidden="true"
+    >
+      <div class="modal-dialog">
+        <div class="modal-content">
+          <div class="modal-header">
+            <h2 class="modal-title fs-5" id="exampleModalLabel">
+              Movie Title: {{ movie.title }}
+            </h2>
+            <button
+              type="button"
+              class="btn-close"
+              data-bs-dismiss="modal"
+              aria-label="Close"
+            ></button>
+          </div>
+          <div class="modal-body">
+            <div class="row">
+              <h4 class="modal-title fs-5 fst-italic" id="exampleModalLabel">
+                Movie tagline: '{{ movie.tagline }}'
+              </h4>
+              <p>Description: {{ movie.overview }}</p>
+              <p>Langauge: {{ movie.original_language }}</p>
+              <p>Release Date: {{ movie.release_date }}</p>
+            </div>
+          </div>
+          <div class="modal-footer">
+            <button
+              type="button"
+              class="btn btn-danger"
+              data-bs-dismiss="modal"
+            >
+              Close
+            </button>
+          </div>
+        </div>
+
+        <!-- Button to trigger the modal -->
+      </div>
+    </div>
+  </div>
 </template>
+
+<script>
+export default {
+  props: ["movie"], // Pass movie details as a prop
+};
+</script>
